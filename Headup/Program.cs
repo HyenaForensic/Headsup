c403e134d4aa8b48e611e362f149f8a801e643eb
DESKTOP-U4BCVO5\HM <ifkiller@hotmail.com>
2021-01-06 16:18:16 +0900

    시작

--- Headup/Program.cs
+++ Headup/Program.cs
@@ -18,7 +18,7 @@ namespace Headup
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main());
-            //Application.Run(new Form1());
+            //Application.Run(new Form1()); 
         }
     }
 }
